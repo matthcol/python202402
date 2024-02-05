f0f4e1c48e8f7db56c01b9eb5baf1ae1fa0a4434
matthcol <matthias.colin@gmail.com>
2024-02-05 14:23:35 +0100

    pytest parametrized

--- Basics/test_euclide.py
+++ Basics/test_euclide.py
@@ -2,11 +2,32 @@ import pytest
 
 from euclide import gcd
 
-def test_gcd():
+def test_gcd_nominal():
     assert gcd(21, 15) == 3
 
 def test_gcd_one_equals_1():
     assert gcd(1, 5) == 1
+
+@pytest.mark.parametrize(
+    ['a', 'b', 'expected_result'], 
+    [
+        (1, 1, 1),
+        (1, 5, 1),
+        (5, 1, 1),
+        (15, 21, 3),
+        (21, 15, 3),
+        (5, 5, 5),
+    ], 
+    ids=[
+        'all 1',
+        'first 1',
+        'second 1',
+        'first lesser than second',
+        'first greater than second',
+        'all equals'
+    ])    
+def test_gcd(a, b, expected_result):
+    assert gcd(a, b) == expected_result
 
 @pytest.mark.skip('Errors not managed yet')
 def test_gcd_one_negative_or_null():
