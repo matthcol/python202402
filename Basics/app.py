b5ed4be7bbfe5f6e558b87df1305e3f84200929c
matthcol <matthias.colin@gmail.com>
2024-02-05 14:39:31 +0100

    add docstring gcd

---
+++ Basics/app.py
@@ -0,0 +1,10 @@
+from euclide import gcd
+
+def main():
+    a = 15
+    b = 21
+    g=gcd(a, b)
+    print('Gcd of', a, 'and', b, 'is', g)
+
+if __name__ == '__main__':
+    main()
--- Basics/euclide.py
+++ Basics/euclide.py
@@ -1,4 +1,13 @@
 def gcd(a, b):
+    """Compute greater common diviser of strictly positive numbers a and b
+
+    Args:
+        a: first number (>0)
+        b: second number (>0)
+
+    Returns:
+        gcd of a and b
+    """
     while a != b:
         if a > b:
             a= a - b
