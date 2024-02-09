19696bee0088f1f649a0cf82f515ab0df5e17dd1
matthcol <matthias.colin@gmail.com>
2024-02-09 13:14:59 +0100

    table city for sqlserver

---
+++ db/sqlserver/sql/cities.sql
@@ -0,0 +1,14 @@
+create table city(
+    id int identity,
+    insee_code char(5), 
+    city_code varchar(50), 
+    zip_code char(5), 
+    label char(5) not null, 
+    latitude double precision, 
+    longitude double precision,
+    department_name varchar(30), 
+    department_number varchar(3), 
+    region_name varchar(30),
+    region_geojson_name varchar(30),
+	constraint pk_city PRIMARY KEY(id)
+);
